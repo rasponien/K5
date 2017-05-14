0b2b3e0fbbb1f266b6cafd3e39be00ebdfd66ed9
Carl Custav Mäeorg <carlmaeorg@hotmail.com>
2017-05-14 19:08:21 +0300

    search query for word added

--- WordPronuncationApplication/urls.py
+++ WordPronuncationApplication/urls.py
@@ -2,9 +2,11 @@ __author__ = 'carlcustav'
 __author__ = 'carlcustav'
 from django.conf.urls import url
 from . import views
-from WordPronuncationApplication.views import IndexView
+from WordPronuncationApplication.views import IndexView, WordView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index')
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'words/(?P<searchWord>.*)^$', WordView.as_view(), name='searchWord')
+
 ]
 
